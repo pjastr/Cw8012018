46061b738763ee631c6d76f0e11deec35811775f
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-08 13:01:00 +0100

    Add files via upload

---
+++ WpfApplication15/WpfApplication15/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            // ... lista
+            List<string> data = new List<string>();
+            data.Add("Book");
+            data.Add("Computer");
+            data.Add("Chair");
+            data.Add("Mug");
+            // ... ustawienie referencji
+            var comboBox = sender as ComboBox;
+            // ... ustawienie zródla
+            comboBox.ItemsSource = data;
+            // ... czy cos domyslnie ma byc wybrane
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // ... dostep do combo.
+            var comboBox = sender as ComboBox;
+            // ... ustawiemy wybraną opcję jako tytul okna
+            string value = comboBox.SelectedItem as string;
+            this.Title = "Selected: " + value; 
+        }
+    }
+}
