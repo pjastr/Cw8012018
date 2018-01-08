6ff39c17eff4cd2c3cad1cfe0d67249241b0e3bc
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-08 13:02:41 +0100

    Add files via upload

---
+++ WpfApplication17/WpfApplication17/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Osoba osoba = new Osoba();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime temp = Data2.SelectedDate.Value.Date;
+            //DateTime.TryParse(DataT.Text, out temp);
+            osoba.UstawDane(ImieT.Text, NazwiskoT.Text, temp);
+            OpisT.Text = osoba.ToString();
+        }
+    }
+}
---
+++ WpfApplication17/WpfApplication17/Osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication17
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        private DateTime dataUrodzenia;
+
+        public Osoba() { }
+
+        public void UstawDane(string imie, string nazwisko, DateTime dataUrodzenia)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.dataUrodzenia = dataUrodzenia;
+        }
+
+        public override string ToString()
+        {
+            return "Imię: "+imie+Environment.NewLine+"Nazwisko: "+nazwisko+Environment.NewLine+"Data urodzenia: "+dataUrodzenia;
+        }
+
+
+    }
+}
