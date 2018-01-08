44569dbff5e2c549c655541ff3b5304b867e6df7
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-08 12:59:20 +0100

    Add files via upload

---
+++ WpfApplication13/WpfApplication13/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Osoba o1 = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            o1 = new Osoba("Jan", 20); //domyślne wartości
+            GridOsoba.DataContext = o1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst = String.Format("{0}{1}{2}", "Wprowadzono dane:", Environment.NewLine, o1.ToString());
+            Wynik.Text = tekst;
+        }
+
+    }
+}
---
+++ WpfApplication13/WpfApplication13/Osoba.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApplication13
+{
+    class Osoba:IDataErrorInfo
+    {
+        public string Imie { get; set; }
+
+        public int Wiek { get; set; }
+
+        public Osoba(string imie, int wiek)
+        {
+            Imie = imie;
+            Wiek = wiek;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Imię: {0}, Wiek: {1}", Imie, Wiek);
+        }
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string komunikat = String.Empty;
+                switch (columnName)
+                {
+                    case "Imie":
+                        if (string.IsNullOrEmpty(Imie))
+                            komunikat = "Imię musi być wpisane!";
+                        else if (Imie.Length < 3)
+                            komunikat = "Imię musi mieć minimum 3 znaki!";
+                        break;
+                    case "Wiek":
+                        if (Wiek < 0)
+                            komunikat = "Wiek musi być dodatni";
+                        break;
+                };
+                return komunikat;
+            }
+        }
+    }
+}
